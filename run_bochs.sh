64d6da86356ab1c3f84bb0b13ea4befadfa3e98f
Alexander Kyte <alexanderkyte@gmail.com>
2012-09-23 15:42:49 -0400

    fixed bochs scipt

--- run_bochs.sh
+++ run_bochs.sh
@@ -3,7 +3,7 @@
 # run_bochs.sh
 # mounts the correct loopback device, runs bochs, then unmounts.
 # Taken from JamesM's tutorial. Nice bochs script. 
-
-sudo /sbin/losetup /dev/loop0 floppy.img
+sudo modprobe loop
+sudo /sbin/losetup /dev/loop0 grubfloppy.img
 sudo bochs -f bochsrc.txt
 sudo /sbin/losetup -d /dev/loop0
--- update_image.sh
+++ update_image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script mounts the boot floppy image, copies the kernel you've written in src/kernel to it, and then unmounts it. 
-sudo losetup /dev/loop0 grubfloppy.img
+mkdir ./grubwrite
 # losetup is a loop device and control toolset. This makes the grub boot floppy mounted at loop device /dev/loop0
-sudo mount /dev/loop0 /mnt
-sudo cp src/kernel /mnt/kernel
-sudo umount /dev/loop0
-sudo losetup -d /dev/loop0 
+sudo mount -o loop src/grubfloppy.img ./grubwrite 
+sudo cp ./src/kernel ./grubwrite/kernel
+sudo umount grubwrite
+rm -r grubwrite
