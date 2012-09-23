af68bef047e5ae6483b1a642ab8343de97290f89
Alexander Kyte <alexanderkyte@gmail.com>
2012-09-23 17:14:52 -0400

    fixin'

--- run_bochs.sh
+++ run_bochs.sh
@@ -4,6 +4,6 @@
 # mounts the correct loopback device, runs bochs, then unmounts.
 # Taken from JamesM's tutorial. Nice bochs script. 
 sudo modprobe loop
-sudo /sbin/losetup /dev/loop0 grubfloppy.img
+sudo /sbin/losetup /dev/loop0 src/grubfloppy.img
 sudo bochs -f bochsrc.txt
 sudo /sbin/losetup -d /dev/loop0
